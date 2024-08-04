219347b316fbda4cf4e6e464b4a705d48582b449
HP <abhinavchauhan744@gmail.com>
2024-08-04 15:11:03 +0530

    seventh commit

--- 10-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
+++ 10-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/com/luv2code/cruddemo/dao/AppDAO.java
@@ -33,4 +33,8 @@ public interface AppDAO {
     Course findCourseById(int theId);
 
     void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewsByCourseId(int theId);
 }
