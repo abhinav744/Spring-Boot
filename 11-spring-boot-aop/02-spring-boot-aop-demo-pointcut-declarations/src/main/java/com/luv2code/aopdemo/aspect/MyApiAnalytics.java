a574a581b2ed7882b76813ca51c1d35cdb8c952a
HP <abhinavchauhan744@gmail.com>
2024-08-11 18:58:28 +0530

    seventh commit

--- 11-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyApiAnalytics.java
+++ 11-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyApiAnalytics.java
@@ -7,15 +7,12 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
-
+@Order(3)
+public class MyApiAnalytics {
 
     @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n=====>>> Executing @Before advice on method");
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Performing API analytics");
     }
-
-
 
 }
