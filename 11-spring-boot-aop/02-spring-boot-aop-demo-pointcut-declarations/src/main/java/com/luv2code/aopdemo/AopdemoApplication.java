9548fc328b267a5b296f118dca612e78956cf516
HP <abhinavchauhan744@gmail.com>
2024-08-13 03:17:39 +0530

    seventh commit

--- 11-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
+++ 11-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/src/main/java/com/luv2code/aopdemo/AopdemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class AopdemoApplication {
 
@@ -19,14 +21,32 @@ public class AopdemoApplication {
 
 		return runner -> {
 
-			demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+			// demoTheBeforeAdvice(theAccountDAO, theMembershipDAO);
+			demoTheAfterReturningAdvice(theAccountDAO);
 		};
+	}
+
+	private void demoTheAfterReturningAdvice(AccountDAO theAccountDAO) {
+
+		// call method to find accounts
+		List<Account> theAccounts = theAccountDAO.findAccounts();
+
+		// display accounts
+		System.out.println("\n\nMain Progress: demoTheAfterReturningAdvice");
+		System.out.println("----");
+
+		System.out.println(theAccounts);
+
+		System.out.println("\n");
 	}
 
 	private void demoTheBeforeAdvice(AccountDAO theAccountDAO, MembershipDAO theMembershipDAO) {
 
 		// call the business method
 		Account myAccount = new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
+
 		theAccountDAO.addAccount(myAccount, true);
 		theAccountDAO.doWork();
 
